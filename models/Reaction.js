23341996f2e7e7476a73670ebba3b2b7cb3a069e
AlbusSeverus <ericsonhnc@gmail.com>
2023-06-13 20:49:48 -0700

    reaction.js under models/ updated

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => createdAtVal.toISOString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction;
