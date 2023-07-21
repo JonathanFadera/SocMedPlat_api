e669f72fb4152dbf8d0b339292e8a6846c191129
AlbusSeverus <ericsonhnc@gmail.com>
2023-07-20 22:07:52 -0700

    server.js updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-const activity = 'social network';
+const activity = 'Social Media Platform';
 
 const PORT = 3001;
 const app = express();
